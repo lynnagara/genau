afd9610258b84c0071b0c33f0eba2002c81dbe2c
Rouven Weßling <rouven@contentful.com>
2017-06-10 13:57:22 +0200

    Handle correct/incorrect answers.

--- components/game.js
+++ components/game.js
@@ -13,15 +13,38 @@ import {
 } from 'react-native';
 import Timer from './timer';
 
-export default class App extends Component {
+export default class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      question: "Scharf",
+      answers: ["Sharp", "Difficult", "Funny", "Amusing"],
+      correctAnswerIndex: 0
+    };
+
+    this.answerSelectedFunctions = this.state.answers.map((value, index) => this.answerSelected.bind(this, index));
+  }
+
+  answerSelected(index) {
+    if (index === this.state.correctAnswerIndex) {
+      console.log('Correct!');
+    } else {
+      console.log('Incorrect!');
+    }
+  }
+
   render() {
+    const answersButtons = this.state.answers.map(function (value, index) {
+      return (<Button title={ value } onPress={this.answerSelectedFunctions[index]} />);
+    }, this);
+
     return (
       <View style={styles.container}>
         <Timer />
-        <Text>Scharf</Text>
-        <Button title="Hot">Hot</Button>
-        <Button title="Difficult">Difficult</Button>
-        <Button title="Funny">Funny</Button>
+        <Text>{ this.state.question }</Text>
+
+        {answersButtons}
       </View>
     );
   }
